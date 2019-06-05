e9893ba29a78df1aac1ab3e413db30c286c4435e
Barry <bbalasingham+git@gmail.com>
2019-06-04 21:27:09 -0700

    [main] adding Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.io.Console;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
     }
 }
